9c4bb957965d70f5fff711d651228a6162eb89c9
Uriel Enrique <urielenrique99@gmail.com>
2025-11-22 21:21:34 -0600

    Adding brewfile support

--- mac_backup.go
+++ mac_backup.go
@@ -7,19 +7,32 @@ import (
 )
 
 func main() {
-	command := exec.Command("sdsd", "--version")
 
-	_, err := command.Output()
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide the path of your brewfile")
+		return
+	}
+
+	command := exec.Command("brew", "--version")
+
+	err := command.Run()
 
 	if err != nil {
 		fmt.Println("Homebrew not installed, installing it now")
-		err := installHomebrew()
+		err = installHomebrew()
 
 		if err != nil {
 			fmt.Errorf("Homebrew installation failed")
+			return
 		}
 
 		fmt.Println("Homebrew installed successfully")
+	}
+
+	fmt.Println("Proceding with the installation of caks/formulae as defined in the brewfile")
+
+	if err = installBrewSoftware(); err != nil {
+		fmt.Println("Something unexpected happened when installing the casks/formulae", err.Error())
 	}
 }
 
@@ -36,6 +49,16 @@ func installHomebrew() error {
 	command = exec.Command("/bin/bash", "-c", string(remoteScript))
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
+
+	return command.Run()
+}
+
+func installBrewSoftware() error {
+	pahtToBrewfile := os.Args[1]
+
+	command := exec.Command("brew", "bundle", "--file", pahtToBrewfile)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
 
 	return command.Run()
 }
